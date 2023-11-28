f0c199469e98fdb3ea38e0eb2b0730233447bf3f
Abdelmalik MEBARKI <abdelmalik.mebarki@viacesi.fr>
2023-11-28 10:27:43 +0100

    Ajoutez des fichiers projet.

---
+++ PROSIT5/CLexplorateur.h
@@ -0,0 +1,17 @@
+#pragma once
+
+using namespace System::IO;
+using namespace System;
+
+namespace NS_composants
+{
+	ref class CLfichier
+	{
+	public:
+		array<String^>^ explorerUnDossier(String^ dossier);
+		void effacer(String^);
+		void copier(String^, String^);
+	};
+}
+
+
---
+++ PROSIT5/CLimages.h
@@ -0,0 +1,13 @@
+#pragma once
+
+using namespace System;
+namespace NS_composants
+{
+	public ref class CLimages
+	{
+	public:
+		System::Drawing::Bitmap^ acquisitionImage(String^);
+	};
+}
+
+
